d1ca6158e9c9f422ec414e6f246e02dce0880e4a
Kin <kinbei@qq.com>
2016-06-12 14:38:58 +0800

    commit

--- hello-new-gate/main.lua
+++ hello-new-gate/main.lua
@@ -1,16 +1,28 @@
 local skynet = require "skynet"
+local socket = require "socket"
+local proxy = require "socket_proxy"
 
-skynet.start( function()
-	skynet.error("Server start")
-	local console = skynet.newservice("console")
+local function read(fd)
+	return skynet.tostring(proxy.read(fd))
+end
+
+skynet.start(function()
 	skynet.newservice("debug_console",8000)
-	
-	local watchdog = skynet.newservice("watchdog")
-	skynet.call(watchdog, "lua", "start", {
-		port = 8888,
-		maxclient = max_client,
-		nodelay = true,
-	})
-	
-	skynet.error("Watchdog listen on ", 8888)
-end )
+	local id = assert(socket.listen("127.0.0.1", 8888))
+	socket.start(id, function (fd, addr)
+		skynet.error(string.format("%s connected as %d" , addr, fd))
+		proxy.subscribe(fd)
+		while true do
+			local ok, s = pcall(read, fd)
+			if not ok then
+				skynet.error("CLOSE")
+				break
+			end
+			if s == "quit" then
+				proxy.close(fd)
+				break
+			end
+			skynet.error(s)
+		end
+	end)
+end)
