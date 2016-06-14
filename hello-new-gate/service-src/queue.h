9225095d0dade1da59ca818139a842685c4f40b8
Kin <kinbei@qq.com>
2016-06-14 09:52:18 +0800

    move queue logic to another files

---
+++ hello-new-gate/service-src/queue.h
@@ -0,0 +1,18 @@
+#ifndef queue_h
+#define queue_h
+
+struct queue {
+	int cap;
+	int sz;
+	int head;
+	int tail;
+	char * buffer;
+};
+
+void queue_init(struct queue *q, int sz);
+int queue_empty(struct queue *q);
+int queue_pop(struct queue *q, void *result);
+void queue_push(struct queue *q, const void *value);
+int queue_size(struct queue *q);
+
+#endif
--- hello-new-gate/service-src/service_package.c
+++ hello-new-gate/service-src/service_package.c
@@ -1,5 +1,6 @@
 #include "skynet.h"
 #include "skynet_socket.h"
+#include "queue.h"
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -15,14 +16,6 @@ struct response {
 struct request {
 	uint32_t source;
 	int session;
-};
-
-struct queue {
-	int cap;
-	int sz;
-	int head;
-	int tail;
-	char * buffer;
 };
 
 struct package {
@@ -41,71 +34,6 @@ struct package {
 	struct queue request;
 	struct queue response;
 };
-
-static void
-queue_init(struct queue *q, int sz) {
-	q->head = 0;
-	q->tail = 0;
-	q->buffer = NULL;
-	q->sz = sz;
-}
-
-static int
-queue_empty(struct queue *q) {
-	return q->head == q->tail;
-}
-
-static int
-queue_pop(struct queue *q, void *result) {
-	if (q->head == q->tail) {
-		return 1;
-	}
-	memcpy(result, q->buffer + q->head * q->sz, q->sz);
-	q->head++;
-	if (q->head >= q->cap)
-		q->head = 0;
-	return 0;
-}
-
-static void
-queue_push(struct queue *q, const void *value) {
-	void * slot = q->buffer + q->tail * q->sz;
-	++q->tail;
-	if (q->tail >= q->cap)
-		q->tail = 0;
-	if (q->head == q->tail) {
-		// full
-		assert(q->sz > 0);
-		int cap = q->cap * 2;
-		if (cap == 0)
-			cap = 4;
-		char * tmp = skynet_malloc(cap * q->sz);
-		int i;
-		int head = q->head;
-		for (i=0;i<q->cap;i++) {
-			memcpy(tmp + i * q->sz, q->buffer + head * q->sz, q->sz);
-			++head;
-			if (head >= q->cap) {
-				head = 0;
-			}
-		}
-		skynet_free(q->buffer);
-		q->head = 0;
-		q->tail = q->cap ? q->cap : 1;
-		q->cap = cap;
-		q->buffer = tmp;
-		slot = tmp;
-	}
-	memcpy(slot, value, q->sz);
-}
-
-static int
-queue_size(struct queue *q) {
-	if (q->head > q->tail) {
-		return q->tail + q->cap - q->head;
-	}
-	return q->tail - q->head;
-}
 
 static void
 service_exit(struct skynet_context *ctx, struct package *P) {
